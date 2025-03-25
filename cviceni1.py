a969176c582a18c7c4c320142d20bfdf45c9335f
unknown <michaela.cekalova@infis.cz>
2025-03-25 08:28:29 +0100

    ukol

---
+++ cviceni1.py
@@ -0,0 +1,12 @@
+#uživatel tohoto programu je doktor, který chce přidávat pacienty do svojí databáze
+#uprav kód pomocí cyklu tak, aby se program nejdřív zeptal uživatele kolik pacientů chce přidat a podle toho pak proběhl kód níže
+
+
+#tento kousek kódu se bude opakovat tolikrát kolikrát zadá uživatel
+
+cyklus = int (input("kolik pacientů chcete přidat"))
+for i in range (cyklus):
+    jmeno = input("Zadejte jméno pacienta, kterého chcete přidat")
+    prijmeni = input("Zadejte příjmení pacienta, kterého chcete přidat")
+    rok = int(input("Zadejte rok narození pacienta ktetého chcete přidat"))
+    print(f"Pacient {jmeno} {prijmeni} rok narození {rok} byl přidán")
